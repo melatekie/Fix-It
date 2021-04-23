96560c3adee622be8aa4898fb76f39c680245ad5
melatekie <wattevah@gmail.com>
2021-04-23 01:45:19 -0400

    sign up activity layout & switch between user & prof

--- app/src/main/java/com/example/fixit/SignUpActivity.java
+++ app/src/main/java/com/example/fixit/SignUpActivity.java
@@ -6,28 +6,53 @@ import androidx.databinding.DataBindingUtil;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
 
 import com.example.fixit.databinding.ActivitySignUpBinding;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
     private MaterialToolbar toolbar;
+    private SwitchMaterial smSwitch;
+    private RelativeLayout rlUser, rlProf;
+    private Button btnUser, btnProf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up);
 
+        smSwitch = binding.smSwitch;
+        rlUser = binding.rlUser;
+        rlProf = binding.rlProfessional;
         toolbar = binding.topAppBar;
         setSupportActionBar(toolbar);
 
+
+        smSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    rlProf.setVisibility(rlProf.VISIBLE);
+                    rlUser.setVisibility(rlUser.GONE);
+                }else {
+                    rlProf.setVisibility(rlProf.GONE);
+                    rlUser.setVisibility(rlUser.VISIBLE);
+                }
+            }
+        });
+
         //onClick for back button
-        binding.topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), LoginActivity.class);
+                Intent i = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(i);
             }
         });
